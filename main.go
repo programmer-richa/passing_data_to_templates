10ee6a290e2b2ba89bf07759b8c01f08be6f86bb
programmer-richa <programmer.richa@gmail.com>
2020-06-02 17:40:13 +0530

    Basic program to show working of templates.

--- main.go
+++ main.go
@@ -13,11 +13,55 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
+type Student struct {
+	Name          string
+	Qualification string
+	Grade         string
+}
+
 func main() {
+	// Passing map to a template
 	data := map[string]string{"title": "Home", "user": "Richa", "topic": "Programming"}
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", data)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Passing slice to a template
+
+	languages := []string{"HTML 5", "CSS 3", "Go", "Javascript"}
+	err = tpl.ExecuteTemplate(os.Stdout, "languages_slice.gohtml", languages)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Passing map to a template
+	student := map[string]string{"Name": "Richa", "Qualification": "MCA", "Grade": "A"}
+	err = tpl.ExecuteTemplate(os.Stdout, "student_map.gohtml", student)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Passing struct to a template
+	s := Student{
+		Name:          "Richa",
+		Qualification: "MCA",
+		Grade:         "A",
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "student_struct.gohtml", s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s1 := Student{
+		Name:          "Sahil",
+		Qualification: "MCA",
+		Grade:         "A",
+	}
+
+	students := []Student{s, s1}
+	err = tpl.ExecuteTemplate(os.Stdout, "student_slice_of_struct.gohtml", students)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
